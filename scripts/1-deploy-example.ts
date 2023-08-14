4145fbccca9f30a09eca26e70200a31e7433b948
markgenuine <mrkgeniy@gmail.com>
2023-08-14 13:08:21 +0300

    refactoring

--- scripts/1-deploy-example.ts
+++ scripts/1-deploy-example.ts
@@ -1,31 +1,18 @@
-import {WalletTypes} from "locklift";
-import prompts from 'prompts';
+import {WalletTypes, toNano} from "locklift";
 
 const isValidEverAddress = (address: any) =>
     /^(?:-1|0):[0-9a-fA-F]{64}$/.test(address);
 
 async function main() {
-    // const response = await prompts([
-    //     {
-    //         type: 'text',
-    //         name: 'owner',
-    //         message: 'Bar owner',
-    //         validate: (value: any) =>
-    //             isValidEverAddress(value) || value === ''
-    //                 ? true
-    //                 : 'Invalid Everscale address',
-    //     },
-    // ]);
 
     const signer = (await locklift.keystore.getSigner("0"))!;
     const { account } = await locklift.factory.accounts.addNewAccount({
         type: WalletTypes.EverWallet,
-        value: locklift.utils.toNano(5),
+        value: toNano(5),
         publicKey: signer.publicKey,
     });
 
     console.log(`Account owner: ${account.address}`);
-    // const ownerBar = response.owner || account;
     const fooCode = await locklift.factory.getContractArtifacts("Foo");
     const {contract: exampleBar,} = await locklift.factory.deployContract({
         contract: "Bar",
@@ -37,7 +24,7 @@ async function main() {
             _owner: account.address,
             _fooContractCode: fooCode.code,
         },
-        value: locklift.utils.toNano(5),
+        value: toNano(5),
     });
 
     console.log(`Bar contract: ${exampleBar.address}`);
--- test/example-test.ts
+++ test/example-test.ts
@@ -1,23 +1,41 @@
-import {Contract, Signer, toNano, WalletTypes, zeroAddress} from "locklift";
-import {BarAbi, FactorySource, FooAbi} from "../build/factorySource";
+import {Contract, getRandomNonce, Signer, toNano, WalletTypes, zeroAddress} from "locklift";
 import {Account} from "everscale-standalone-client";
 import {expect} from "chai";
+import {GasValuesAbi} from "../build/factorySource";
+import { calcValue } from "../scripts/utils";
 
 let signer: Signer;
 let owner:Account;
+let gasValues: Contract<GasValuesAbi>;
 
 const fooCode = locklift.factory.getContractArtifacts("Foo");
+
 describe("Test example", async function (){
     before(async() => {
         signer = (await locklift.keystore.getSigner("0"))!;
 
         const {account} = await locklift.factory.accounts.addNewAccount({
             type: WalletTypes.EverWallet,
-            value: locklift.utils.toNano(5),
+            value: toNano(5),
             publicKey: signer.publicKey,
         });
 
         owner = account;
+
+        const {contract: gasValue,} = await locklift.factory.deployContract({
+            contract: "GasValues",
+            publicKey: signer.publicKey,
+            initParams: {
+                _nonce: getRandomNonce()
+            },
+            constructorParams: {
+                owner_: owner.address
+            },
+            value: toNano(2)
+        });
+
+        gasValues = gasValue
+
     });
 
     it("Deploy Bar contract and check Foo contract", async function(){
@@ -31,21 +49,31 @@ describe("Test example", async function (){
                 _owner: owner.address,
                 _fooContractCode: fooCode.code,
             },
-            value: locklift.utils.toNano(2),
+            value: toNano(2),
         });
 
         expect(await locklift.provider.getBalance(barContract.address).then(balance => Number(balance))).to.be.above(0, 'Balance should be > 0');
         expect((await barContract.methods.getDetails({answerId: 0}).call()).value0.owner.toString()).to.be.equal(owner.address.toString(), 'Wrong owner address Bar contract');
         expect((await barContract.methods.getDetails({answerId: 0}).call()).value0.fooContract.toString()).to.be.equal(zeroAddress.toString(), 'Wrong address foo contract, should be 0');
 
-        await barContract.methods.deployFooContract().send({amount: toNano(2), from: owner.address});
+        const gasDeployFoo = await gasValues.methods
+            .getDeployFooValue({})
+            .call({})
+            .then(r => r.value0);
+
+        await barContract.methods.deployFooContract().send({amount: calcValue(gasDeployFoo), from: owner.address});
         const fooContract = await locklift.factory.getDeployedContract("Foo", ((await barContract.methods.getDetails({answerId: 0}).call()).value0.fooContract));
-        expect(fooContract).not.be.equal(zeroAddress, 'Wrong address foo contract, not should be 0')
+        expect(fooContract.address.toString()).not.be.equal(zeroAddress.toString(), 'Wrong address foo contract, not should be 0')
 
         expect((await fooContract.methods.getActive({answerId: 0}).call()).value0.valueOf()).to.be.equal(false, 'Active shuold be false');
 
+        const gasSetActive = await gasValues.methods
+            .getSetActiveFooValue({})
+            .call({})
+            .then(r => r.value0);
+
         await barContract.methods.setActiveFoo({_active: true}).send({
-            amount: toNano(1), from: owner.address
+            amount: calcValue(gasSetActive), from: owner.address
         });
 
         expect((await fooContract.methods.getActive({answerId: 0}).call()).value0).to.be.equal(true,'Active shuold be true');
